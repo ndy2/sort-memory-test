a4ad5f8db8b33ebc9e272ac1d9ee39e60f581d84
ndy2 <emrdbs12@gmail.com>
2023-01-31 01:51:53 +0900

    [group 6 & 7 추가]

--- src/KotlinTests.kt
+++ src/KotlinTests.kt
@@ -2,6 +2,7 @@ package tests
 
 import test.SortTest
 
+@Suppress("UNCHECKED_CAST")
 class KotlinTests {
     companion object {
         // [group 1 - comparable]
@@ -23,9 +24,121 @@ class KotlinTests {
         val kotlinExtractedInlineCompareValuesTest =
             SortTest { (it as ArrayList).sortWith(Comparator { a, b -> compareValues(a.a, b.a) }) }
 
-        // [group 5 - extracted inline comparable casting tests]
-        @Suppress("UNCHECKED_CAST")
-        val kotlinExtractedInlineComparableCastingTest =
-            SortTest { (it as ArrayList).sortWith(Comparator { a, b -> (a.a as Comparable<Any>).compareTo(b.a) }) }
+        // [group 5 - my compare values with === comparison]
+        val kotlinMyNullableCompareValues1Test =
+            SortTest { (it as ArrayList).sortWith(Comparator { a, b -> myNullableCompareValues1(a.a, b.a) }) }
+
+        val kotlinMyNotNullableCompareValues1Test =
+            SortTest { (it as ArrayList).sortWith(Comparator { a, b -> myNotNullableCompareValues1(a.a, b.a) }) }
+
+        val kotlinMyNotNullableCompareValues2Test =
+            SortTest { (it as ArrayList).sortWith(Comparator { a, b -> myNotNullableCompareValues2(a.a, b.a) }) }
+
+
+        // [group 6 - my compare values with no === comparison]
+        val kotlinMyNullableCompareValues2Test =
+            SortTest { (it as ArrayList).sortWith(Comparator { a, b -> myNullableCompareValues2(a.a, b.a) }) }
+
+        val kotlinMyNotNullableCompareValues3Test =
+            SortTest { (it as ArrayList).sortWith(Comparator { a, b -> myNotNullableCompareValues3(a.a, b.a) }) }
+
+        val kotlinMyNotNullableCompareValues4Test =
+            SortTest { (it as ArrayList).sortWith(Comparator { a, b -> (a as Comparable<Any>).compareTo(b) }) }
+
+        // [group 7 - int parameters]
+        val kotlinIntParameters1 =
+            SortTest { (it as ArrayList).sortWith(Comparator { a, b -> comparingInt1(a.a, b.a) }) }
+
+        val kotlinIntParameters2 =
+            SortTest { (it as ArrayList).sortWith(Comparator { a, b -> comparingInt2(a.a, b.a) }) }
+
+        val kotlinIntParameters3 =
+            SortTest { (it as ArrayList).sortWith(Comparator { a, b -> comparingInt3(a.a, b.a) }) }
+
+        val kotlinIntParameters4 =
+            SortTest { (it as ArrayList).sortWith(Comparator { a, b -> comparingInt4(a.a, b.a) }) }
+
+
+        // [group 5 - my compare values with === comparison] helper methods
+        // ComparisonsKt.comparesValues 와 똑같다.
+        private fun <T : Comparable<*>> myNullableCompareValues1(a: T?, b: T?): Int {
+            if (a === b) return 0
+            if (a == null) return -1
+            if (b == null) return 1
+
+            return (a as Comparable<Any>).compareTo(b)
+        }
+
+        private fun <T : Comparable<*>> myNotNullableCompareValues1(a: T, b: T): Int {
+            if (a === b) return 0
+            if (a == null) return -1
+            if (b == null) return 1  // 여기 null 비교는 넣어도 상관 없다 always false!
+
+            return (a as Comparable<Any>).compareTo(b)
+        }
+
+        private fun <T : Comparable<*>> myNotNullableCompareValues2(a: T, b: T): Int {
+            if (a === b) return 0
+            return (a as Comparable<Any>).compareTo(b)
+        }
+
+        // [group 6 - my compare values with 'no' === comparison] helper methods
+        private fun <T : Comparable<*>> myNullableCompareValues2(a: T?, b: T?): Int {
+            if (a == null) return -1
+            if (b == null) return 1
+
+            return (a as Comparable<Any>).compareTo(b)
+        }
+
+        private fun <T : Comparable<*>> myNotNullableCompareValues3(a: T, b: T): Int {
+            return (a as Comparable<Any>).compareTo(b)
+        }
+
+
+        // [group 7 Int parameters]
+        // 문제 있음 - auto boxing 발생함 + reference 비교 함
+        private fun comparingInt1(a: Int?, b: Int?): Int {
+            if (a === b) return 0
+            if (a == null) return -1
+            if (b == null) return 1
+
+            return (a as Comparable<Any>).compareTo(b)
+        }
+
+        // 문제 없음 - auto boxing 발생함, null 체크 만 함 (reference 비교는 안 함)
+        private fun comparingInt2(a: Int?, b: Int?): Int {
+            if (a == null) return -1
+            if (b == null) return 1
+
+            return (a as Comparable<Any>).compareTo(b)
+        }
+
+
+
+        // note - For values represented by primitive types at runtime (for example, Int),
+        // the === equality check is equivalent to the == check.
+        //
+        // see -  https://kotlinlang.org/docs/equality.html#referential-equality
+        // 문제 없음 (autoboxing 없음)
+        private fun comparingInt3(a: Int, b: Int): Int {
+            if (a === b) return 0
+            if (a == null) return -1 // always false
+            if (b == null) return 1
+
+            return (a as Comparable<Any>).compareTo(b)
+        }
+
+
+
+        // 문제 없음 (auto boxing 없음)
+        private fun comparingInt4(a: Int, b: Int): Int {
+            if (a == null) return -1
+            if (b == null) return 1 // always false
+
+            return (a as Comparable<Any>).compareTo(b)
+        }
+
+
     }
+
 }
--- src/SortMemoryTestMain.kt
+++ src/SortMemoryTestMain.kt
@@ -17,17 +17,30 @@ fun main() {
         "group 2 - java comparator" to JavaTests.javaComparatorTest,
         "group 2 - kotlin comparator" to KotlinTests.kotlinComparatorTest,
 
-        // [group 3 -  inline tests]
+        // [group 3 - inline comparator]
         "group 3 - kotlin inline sortBy" to KotlinTests.kotlinInlineSortByTest,
         "group 3 - kotlin inline compareBy" to KotlinTests.kotlinInlineCompareByTest,
 
-        // [group 4 - extracted inline tests]
-        "group 4 - kotlin extracted inline comparing values by test" to KotlinTests.kotlinExtractedInlineComparingValuesByTest,
-        "group 4 - kotlin extracted inline compare values test" to KotlinTests.kotlinExtractedInlineCompareValuesTest,
+        // [group 4 - extracted inline comparator]
+        "group 4 - kotlin extracted inline comparing values by" to KotlinTests.kotlinExtractedInlineComparingValuesByTest,
+        "group 4 - kotlin extracted inline compare values" to KotlinTests.kotlinExtractedInlineCompareValuesTest,
 
-        // [group 5 - extracted inline comparable casting tests]
-        "group 5 - kotlin extracted inline comparable casting test" to KotlinTests.kotlinExtractedInlineComparableCastingTest,
-    )
+        // [group 5 - my compare values with === comparison]
+        "group 5 - kotlin my nullable compare values1 test" to KotlinTests.kotlinMyNullableCompareValues1Test,
+        "group 5 - kotlin my not nullable compare values1" to KotlinTests.kotlinMyNotNullableCompareValues1Test,
+        "group 5 - kotlin my not nullable compare values2" to KotlinTests.kotlinMyNotNullableCompareValues2Test,
+
+        // [group 6 - my compare values with no === comparison]
+        "group 6 - kotlin my nullable compare values2" to KotlinTests.kotlinMyNullableCompareValues2Test,
+        "group 6 - kotlin my not nullable compare values3" to KotlinTests.kotlinMyNotNullableCompareValues3Test,
+        "group 6 - kotlin my not nullable compare values4" to KotlinTests.kotlinMyNotNullableCompareValues4Test,
+
+        // [group 7 - Int, Int?]
+        "group 7 - kotlin comparing int 1" to KotlinTests.kotlinIntParameters1,
+        "group 7 - kotlin comparing int 2" to KotlinTests.kotlinIntParameters2,
+        "group 7 - kotlin comparing int 3" to KotlinTests.kotlinIntParameters3,
+        "group 7 - kotlin comparing int 4" to KotlinTests.kotlinIntParameters4,
+        )
         .map { (name, test) -> PrintUsedMemorySortTest(name, test) }
 
     repeat(10) {
